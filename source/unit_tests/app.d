87ada37ee63ff17ca160c22f152c63ee3877acd6
Selim Ozel <sozel@wpi.edu>
2021-06-07 00:56:34 +0200

    Ported some of the functions. Stopped at overloading index access operators.

--- source/unit_tests/app.d
+++ source/unit_tests/app.d
@@ -1,64 +1,58 @@
 import std.stdio;
-import matrix;
+import matrixd;
 
-void main()
-{
+void main() {
 	writeln("Starting matrix tests ...");
-}
 
-/*
-int main()
-{
-	std::cout<< "Linear Algebra Tests:\n\n";
-	std::cout<< "Starting matrix tests...\n\n";
-
-	// Assignment and creator operations
-	Matrix A(2, 2, 1.0); // Initialize all elements to 1.
-	A = {{2,3},{3,2}}; // Set elements
+	Matrix A = new Matrix(2, 2, 1.0); // 2x2 matrix with all elements 1.
+	A = [[2,3],[3,2]]; // overload = 
 	assert(A.Size()[0] == 2 && A.Size()[1] == 2);
+	writeln(PrintMatrix(A));
 
-	Matrix::Print(A);
-	std::cout<<"\n";
+	A = [[7,8],[8,7],[8,9]]; // change dimensions
+	assert(A.Size()[0] == 3 && A.Size()[1] == 2); // assert the dimension change
 
-	A = {{7,8},{8,7},{8,9}}; // Set elements and reshape
-	assert(A.Size()[0] == 3 && A.Size()[1] == 2);
+	Matrix B = new Matrix(3, 2, 0.0); // second matrix
+	B = [[3,2],[2,3],[2,1]]; // keeps the dimension same. only sets elements
+	assert(B.Size()[0] == 3 && B.Size()[1] == 2);	
 
-	Matrix B(3, 2, 0.0); 
-	B = {{3,2},{2,3},{2,1}}; // Set elements
-	assert(B.Size()[0] == 3 && B.Size()[1] == 2);
-
-	Matrix C(1, 1, 0.0); // Size can be easily changed later on
+	Matrix C = new Matrix(1, 1, 0.0); // Size change automatically in the next line
 	C = B + A; // Matrix addition
 	assert(C.Size()[0] == 3 && C.Size()[1] == 2);
 	assert(C.Sum() == 60);
 	assert(C.Sum(0) == 20);
 	assert(C.Sum(1) == 20);
-	assert(C.Sum(2) == 20);
-
-	Matrix::Print(C);
-	std::cout<<"\n";
+	assert(C.Sum(2) == 20);	
+	writeln(PrintMatrix(C));
 
 	A = C; // Matrix assignment	
-	assert(A.Size()[0] == 3 && A.Size()[1] == 2);
+	assert(A.Size()[0] == 3 && A.Size()[1] == 2);	
 
 	// Addition-Subtraction-Multiplication-Division(scalar)
-	B = {{3,2},{2,3}};
-	A = {{2,3},{3,2}}; // Set elements
-	assert(A.Size()[0] == B.Size()[0] && A.Size()[1] == B.Size()[1]);
-	
+	B = [[3,2],[2,3]];
+	A = [[2,3],[3,2]]; // Set elements
+	assert(A.Size()[0] == B.Size()[0] && A.Size()[1] == B.Size()[1]);	
+
 	A += B; // cumulative matrix addition
 	A += 1;	// cumulative scalar addition
 	assert(A.Sum(0) + A.Sum(1) == A.Sum() && A.Sum() == 24);
+	writeln(PrintMatrix(A));
 
 	A -= 1; // cumulative matrix subtraction
 	A = A - B; // matrix subtraction
-	A = A - 1; // cumulative scalar subtraction
+	A = A - 1; // scalar subtraction
 	assert(A.Sum(0) + A.Sum(1) == A.Sum() && A.Sum() == 6);
 
-	A = {{3,2,1},{1,2,3},{0,1,2}}; // 3x3
-	B = {{3,2},{1,4},{1,5}}; // 3x2
+	A = [[3,2,1],[1,2,3],[0,1,2]]; // 3x3
+	B = [[3,2],[1,4],[1,5]]; // 3x2
 	A *= B;
 	B = A.T();
+	//A[0][1];
+}
+
+/*
+int main()
+{
 	assert(A(0,0) + A(0,1) == A.Sum(0));
 	assert(A(1,0) + A(1,1) == A.Sum(1));
 	assert(A(2,0) + A(2,1) == A.Sum(2)); // row sum verifications
